e7ba797da89141a6139bc25825848ffdc90378a2
delsa21 <A01644911@tec.mx>
2025-06-05 09:19:05 -0600

    Commit final

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,68 +1,72 @@
 import userModel from "../modules/userModel.js";
 
 const userController = {
-    getAllUsers: async(req, res) => {
-        try {
-            const user = await userModel.getAllUsers();
-            res.status(200).json(user);
-        }
-        catch (error) {
-            console.log("Error ferching users:", error);
-            res.status(500).json({ error: 'Internal Server Error' });
-        }
-    },
-    getUserById: async(req, res) => {
-        const {id} = req.params;
-        try {
-            const user = await userModel.getUserById(id);
-            res.status(200).json(user)
-        } catch (error) {
-            console.log("Error ferching users:", error);
-            res.status(500).json({ error: 'Internal Server Error' });
-        }
-    },
-    getIdByMail: async(req, res) => {
-        const {mail} = req.params;
-        try {
-            const user = await userModel.getIdByMail(mail);
-            res.status(200).json(user)
-        } catch (error) {
-            console.log("Error ferching users:", error);
-            res.status(500).json({ error: 'Internal Server Error' });
-        }
-    },
-    getPasswordByMail: async(req, res) => {
-        const {mail} = req.params;
-        try {
-            const user = await userModel.getPasswordByMail(mail);
-            res.status(200).json(user)
-        } catch (error) {
-            console.log("Error ferching users:", error);
-            res.status(500).json({ error: 'Internal Server Error' });
-        }
-    },
-    createUser: async (req, res) => {
-        const userData = req.body;
-        console.log(req.body);
-        try {
-            const newUser = await userModel.createUser(userData);
-            res.status(201).json(newUser);
-        } catch (error) {
-            console.error('Error fetching users:', error);
-            res.status(500).json({ error: 'Internal Server Error' });
-        }
-    },
-    updateUser: async (req, res) => {
-        const { id } = req.params;
-        const userData = req.body;
-        try {
-            const updateUser = await userModel.updateUser(id, userData);
-            res.status(200).json(updateUser);
-        } catch (error) {
-            console.error('Error fetching users:', error);
-            res.status(500).json({ error: 'Internal Server Error' });
-        }
-    },
-}
+  getAllUsers: async (req, res) => {
+    try {
+      const user = await userModel.getAllUsers();
+      res.status(200).json(user);
+    } catch (error) {
+      console.log("Error ferching users:", error);
+      res.status(500).json({ error: "Internal Server Error" });
+    }
+  },
+  getUserById: async (req, res) => {
+    const { id } = req.params;
+    try {
+      const user = await userModel.getUserById(id);
+      res.status(200).json(user);
+    } catch (error) {
+      console.log("Error ferching users:", error);
+      res.status(500).json({ error: "Internal Server Error" });
+    }
+  },
+  getIdByMail: async (req, res) => {
+    const { mail } = req.params;
+    try {
+      const user = await userModel.getIdByMail(mail);
+      res.status(200).json(user);
+    } catch (error) {
+      console.log("Error ferching users:", error);
+      res.status(500).json({ error: "Internal Server Error" });
+    }
+  },
+  getPasswordByMail: async (req, res) => {
+  const { mail } = req.params;
+  try {
+    const user = await userModel.getPasswordByMail(mail);
+    if (user) {
+      res.status(200).json(user); // { password: "temporal123" }
+    } else {
+      res.status(404).json({ error: "Usuario no encontrado" });
+    }
+  } catch (error) {
+    console.error("Error al obtener contraseña:", error);
+    res.status(500).json({ error: "Error al consultar la base de datos" });
+  }
+},
+
+  createUser: async (req, res) => {
+    const userData = req.body;
+    console.log(req.body);
+    try {
+      const newUser = await userModel.createUser(userData);
+      res.status(201).json(newUser);
+    } catch (error) {
+      console.error("Error fetching users:", error);
+      res.status(500).json({ error: "Internal Server Error" });
+    }
+  },
+  updateUser: async (req, res) => {
+    const { id } = req.params;
+    const userData = req.body;
+    try {
+      const updateUser = await userModel.updateUser(id, userData);
+      res.status(200).json(updateUser);
+    } catch (error) {
+      console.error("Error fetching users:", error);
+      res.status(500).json({ error: "Internal Server Error" });
+    }
+  },
+};
 
 export default userController;
